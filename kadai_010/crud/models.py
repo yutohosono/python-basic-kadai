a2fdb924c6d52075ad370eac035c1211f81a187c
yutohosono <132530191+yutohosono@users.noreply.github.com>
2025-11-01 21:54:04 +0900

    Add files via upload

---
+++ kadai_010/crud/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('list')
